7eaec5855c5073f386c949069e540a268f53052d
- <->
2018-08-22 21:08:56 +0700

    Edit: add "Enter laiSuat(theo nam)"

--- src/CG_Loop/TinhTienLaiChoVay.java
+++ src/CG_Loop/TinhTienLaiChoVay.java
@@ -6,14 +6,17 @@ public class TinhTienLaiChoVay {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Moi ban nhap vao so tien can gui x:");
-        Double x;
+        double x;
         x= sc.nextDouble();
         Scanner sc1= new Scanner(System.in);
         System.out.println("Moi ban nhap so thang can gui month:");
         int month;
         month=sc1.nextInt();
         double tienLai;
-        double laiSuatNam=6.5/100;
+        Scanner sc2= new Scanner(System.in);
+        System.out.println("Moi ban nhap lai suat(theo nam):");
+        double laiSuatNam;
+        laiSuatNam=sc2.nextDouble();
         for(;month<=12;month++){
             tienLai=x*laiSuatNam/12*month;
             System.out.println("Tien lai ma ban nhan duoc sau " + month +" thang la :" + tienLai);
